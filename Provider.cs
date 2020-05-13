054a9171a9042f108e3092190b30a2a8a29ffb5f
Diksha Pande <dikshapande@Dikshas-MacBook-Air.local>
2020-05-13 15:44:14 -0700

    Adding Provider Type in Provider class

--- Provider.cs
+++ Provider.cs
@@ -1,6 +1,60 @@
 using System;
-public enum Provider {
+public enum ProviderType {
 
-  INDIVIDUAL,
-  ORGANIZATION
+  INDIVIDUAL = 0,
+  ORGANIZATION = 1
+}
+
+public class Provider {
+
+  private int providerid = Guid.NewGuid().ToString()
+  private String name;
+  private ProviderType type;
+  private ArrayList inv = new ArrayList();
+
+  public Provider(String name, String type)
+  {
+    id = providerid;
+    this.name = name;
+    if(type.equals("organization"))
+    {
+      this.type = ProviderType.ORGANIZATION;
+    }
+    else {
+      this.type = ProviderType.INDIVIDUAL;
+    }
+  }
+  public ArrayList<string> getInfo(){
+    ArrayList provider = new ArrayList<string>();
+    provider.add(this.id.toString());
+    provider.add(this.name);
+    provider.add(this.type.name());
+    return provider;
+  }
+
+  public void addItem(Item item){
+    Guid guid = Guid.NewGuid();
+    string item = guid.ToString();
+    //see if we already have an item list for current code (key)
+    //if not create one and put it in the map
+    ArrayList<Item> itemList = donatedSold.computeIfAbsent(item.getCode(), k -> new ArrayList<>());
+    itemList.add(item);
+  }
+  // check if item is PLU or not, using safe operation
+  public boolean isItemPlu(boolean itemType){
+    return Boolean.TRUE.equals(itemType);
+  }
+
+
+  public String getType(boolean plu){
+    if(plu){
+      return "PLU";
+    }
+    else{
+      return "UPC";
+    }
+  }
+
+  }
+
 }
