19dc7c63a7fd472739f4965845c4808de840aa00
Diksha Pande <dikshapande@Dikshas-MacBook-Air.local>
2020-05-15 14:50:31 -0700

    Changes in checkout class

--- Checkout.cs
+++ Checkout.cs
@@ -4,12 +4,32 @@ using System.Linq;
 using System.Web;
 using System.Collections;
 
+public class Checkout{
 
+  private static Inventory inventory = null;
+	ArrayList inv = new ArrayList();
+  var myList = new List<KeyValuePair<string, Item>>();
+  private ItemType type;
+  private string itemName;
+  private int itemCode;
+  private int checkoutID;
+  private DateTime rentaldate;
+  private DateTime expirydate;
 // return rented item
 public void returnRentedItem(String code, double qty){
   Checkout co = new Checkout();
   co.returnItem(code, qty);
 }
+public void setCheckoutID(int id)
+{
+  checkoutID = id;
+
+}
+public int getCheckoutID()
+{
+  return checkoutID;
+}
+
 // Create a new checkout, get student's complete cart, and checkout
 // everything. Then add the checkout to the transaction history
 public void checkoutItems(){
@@ -18,7 +38,7 @@ public void checkoutItems(){
   co.checkoutAll();
   transactionHistory.add(co);
 }
-
+}
 public class RegularOrder : OrderBase
 {
     public override double CalculateTotalOrderPrice()
