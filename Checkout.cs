6cff480526087bf0b7df0882847c15ac4d633b74
Diksha Pande <dikshapande@Dikshas-MacBook-Air.local>
2020-05-15 15:16:44 -0700

    Changes in checkout class

--- Checkout.cs
+++ Checkout.cs
@@ -20,11 +20,13 @@ public void returnRentedItem(String code, double qty){
   Checkout co = new Checkout();
   co.returnItem(code, qty);
 }
+//Set a unique ID for Checkout
 public void setCheckoutID(int id)
 {
   checkoutID = id;
 
 }
+//Get the unique checkout id
 public int getCheckoutID()
 {
   return checkoutID;
@@ -38,22 +40,6 @@ public void checkoutItems(){
   co.checkoutAll();
   transactionHistory.add(co);
 }
-}
-public class RegularOrder : OrderBase
-{
-    public override double CalculateTotalOrderPrice()
-    {
-        Console.WriteLine("Calculating the total price of a regular order");
-        return products.Sum(x => x.Price);
-    }
-}
-public class Preorder : OrderBase
-{
-    public override double CalculateTotalOrderPrice()
-    {
-        Console.WriteLine("Calculating the total price of a preorder.");
-        return products.Sum(x => x.Price) * 0.9;
-    }
 }
 
 class New
